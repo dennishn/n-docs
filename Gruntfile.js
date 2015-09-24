5d3d5ef20941191462917371fa6128f71ff36894
Dennis Haulund Nielsen <dhni@nodes.dk>
2015-09-24 08:24:11 +0200

    Apparently it was on all platforms
    Because Assemble was not directly a grunt-enabled module, Gruntfile’s
    load-grunt-config required enabling jitGrunt. Words words words jit
    build grunt grunt assemble jit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,36 @@
 module.exports = function(grunt) {
-  // Define project configuration
-  var project = {
-    paths: {
-      get config() {
-        return this.grunt + 'config/';
-      },
-      dist: 'dist/',
-      doc: 'doc/',
-      grunt: 'grunt/',
-      js: 'js/',
-      scss: 'scss/',
-      vendor: grunt.file.readJSON('.bowerrc').directory + '/'
-    },
-    files: {
-      get config() {
-        return project.paths.config + '*.js';
-      },
-      grunt: 'Gruntfile.js',
-      js: ['js/foundation/foundation.js', 'js/foundation/*.js'],
-      scss: ['scss/nodes.scss']
-    },
-    pkg: grunt.file.readJSON('package.json')
-  };
+	// Define project configuration
+	var project = {
+		paths: {
+			get config() {
+				return this.grunt + 'config/';
+			},
+			dist: 'dist/',
+			doc: 'doc/',
+			grunt: 'grunt/',
+			js: 'js/',
+			scss: 'scss/',
+			vendor: grunt.file.readJSON('.bowerrc').directory + '/'
+		},
+		files: {
+			get config() {
+				return project.paths.config + '*.js';
+			},
+			grunt: 'Gruntfile.js',
+			js: ['js/foundation/foundation.js', 'js/foundation/*.js'],
+			scss: ['scss/nodes.scss']
+		},
+		pkg: grunt.file.readJSON('package.json')
+	};
 
-  // Load Grunt configurations and tasks
-  require('load-grunt-config')(grunt, {
-    configPath: require('path').join(process.cwd(), project.paths.config),
-    data: project
-  });
+	// Load Grunt configurations and tasks
+	require('load-grunt-config')(grunt, {
+		configPath: require('path').join(process.cwd(), project.paths.config),
+		data: project,
+		jitGrunt: {
+			staticMappings: {
+			}
+		}
+	});
 
-  //grunt.loadNpmTasks('grunt-browser-sync');
 };
